fd763d32d3cb167bd03c0e6ed246d37a09a890b8
Jeremy Boyd <boydjj@gmail.com>
2018-10-25 14:43:04 -0500

    Add a function-based echo view

--- httpbucket/app.py
+++ httpbucket/app.py
@@ -9,11 +9,32 @@ logger = logging.getLogger(__name__)
 api = responder.API()
 
 
-@api.route('/{greeting}/{target}')
-def greet(req, resp, *, greeting, target):
-    logger.info('Inside request handler')
-    resp.text = f'{greeting}, {target}!'
+@api.route('/get')
+def echo(req, resp):
+    response = {'url': req.full_url}
+
+    args = {}
+    for k, v in req.params.items_list():
+        if len(v) == 1:
+            args[k] = v[0]
+        else:
+            args[k] = v
+
+    if args:
+        response['args'] = args
+
+    headers = {}
+
+    for k, v in req.headers.items():
+        logger.info(f'{k}, {v}')
+        headers[k.title()] = v
+
+    if req.headers:
+        response['headers'] = headers
+
+    resp.media = response
 
 
 if __name__ == '__main__':
+    logger.info('Kicking off new app...')
     api.run()
