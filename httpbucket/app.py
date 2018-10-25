0a8ce47f71420d1259749fb56ed68f3c0f019013
Jeremy Boyd <boydjj@gmail.com>
2018-10-25 17:38:44 -0500

    Add OpenAPI specification for the API

--- httpbucket/app.py
+++ httpbucket/app.py
@@ -2,13 +2,22 @@ import logging
 import time
 
 import aiohttp
+import marshmallow
 import responder
 
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 
-api = responder.API()
+api = responder.API(title='EchoService', version='0.0.0', openapi='3.0')
+
+
+@api.schema('Echo')
+class EchoSchema(marshmallow.Schema):
+    url = marshmallow.fields.Str()
+    args = marshmallow.fields.Dict()
+    headers = marshmallow.fields.Dict()
+    my_ip = marshmallow.fields.Str()
 
 
 @api.route('/get')
@@ -30,6 +39,16 @@ class EchoView:
                 return await resp.text()
 
     async def on_get(self, req, resp):
+        """A simple echo server endpoint.
+        ---
+        get:
+            description: Echo back request details
+            responses:
+                200:
+                    description: An echo of the request sent to the server
+                    schema:
+                        $ref = "#/components/schemas/Echo"
+        """
         response = {'url': req.full_url}
 
         args = {}
@@ -53,7 +72,7 @@ class EchoView:
         my_ip = await self.get_my_ip()
         response['my_ip'] = my_ip.strip()
         self.increment_counter()
-        resp.media = response
+        resp.media = EchoSchema().dump(response)
 
     def __del__(self):
         logger.info(f'Killing instance of EchoView, counter={EchoView.counter}')
